dfcd9490c53232d22943af8742dc63bd4ade59f8
Andrew Dillon <majik895@gmail.com>
2019-05-14 16:09:08 -0400

    aadded alert

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
 const header = document.querySelector('.main-navigation');
 header.addEventListener('mouseover', function() {
-    header.style.backgroundColor = 'lightgrey'
+    header.style.backgroundColor = 'dodgerblue'
 })
 
 const footeralert = document.querySelector('.footer');
 footeralert.addEventListener('click', function(event) {
-    alert('Copyright Lambda');
-    console.log("is this working")
+    alert('Copyright: CSS Cowboy');
+    console.log("Can You Hear Me Now")
 });
 
 const navContent = document.querySelectorAll('.nav-link');
@@ -18,22 +18,22 @@ navContent.forEach(navItem => {
 
 const highlight1 = document.querySelector('.intro');
 highlight1.addEventListener('mouseover', function() {
-    highlight1.style.backgroundColor = 'lightgreen';
+    highlight1.style.backgroundColor = 'hotpink';
 })
 
 const highlight2 = document.querySelector('.content-section');
 highlight2.addEventListener('mouseover', function() {
-    highlight2.style.backgroundColor = 'lightgreen';
+    highlight2.style.backgroundColor = 'hotpink';
 })
 
 const highlight3 = document.querySelector('.inverse-content');
 highlight3.addEventListener('mouseover', function() {
-    highlight3.style.backgroundColor = 'lightgreen';
+    highlight3.style.backgroundColor = 'hotpink';
 })
 
 const highlight4 = document.querySelector('.content-destination');
 highlight4.addEventListener('mouseover', function() {
-    highlight4.style.backgroundColor = 'lightgreen';
+    highlight4.style.backgroundColor = 'hotpink';
 })
 
 const buttons = document.getElementsByClassName('.destination .btn');
