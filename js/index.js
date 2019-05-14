051261a8c7ec53d220b4fe14a9ed7e1c4d020564
Andrew Dillon <majik895@gmail.com>
2019-05-14 13:59:21 -0400

    First commit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+$('#infobox-toggle').click(function() {
+    $("#cover").show();
+});
+
+$('#close-infobox').click(function() {
+    $("#cover").hide();
+});
+
+var cover = $('#cover');
+cover.click(function(event) {
+    e = event || window.event;
+    if (e.target == this) {
+        cover.hide();
+    }
+});
